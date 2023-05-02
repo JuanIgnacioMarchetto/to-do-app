b77a714154587876474f40b56f86d58da85b5fe7
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-05-02 09:14:41 -0300

    //2/5/2023-914.

--- main.js
+++ main.js
@@ -6,5 +6,13 @@ const botonEnter= document.querySelector('#boton-enter');
 
 
 funtion agregarTarea(tarea){
-  
+  const elemento = 
+  `
+                        <li id="elemento">
+                        <i class="far ${REALIZADO}" data="realizado" id="${id}"></i>
+                        <p class="text ${LINE}">${tarea}</p>
+                        <i class="fas fa-trash de" data="eliminado" id="${id}"></i> 
+                        </li>
+  `
+  lista.insertAdjacentHTML("beforeend",elemento)
 }
