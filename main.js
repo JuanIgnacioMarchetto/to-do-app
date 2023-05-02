4806a24c33b2e8fd2a1e2e31ede8a9dcb4a991fd
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-05-02 09:52:49 -0300

    //2/5/2023-9.43

--- main.js
+++ main.js
@@ -1,38 +1,35 @@
-const lista= document.querySelector('#lista');
+const LIST = [];
+let id = 0;
 
-const input= document.querySelector('#input');
+const lista = document.querySelector('#lista');
+const input = document.querySelector('#input');
+const botonEnter = document.querySelector('#boton-enter');
 
-const botonEnter= document.querySelector('#boton-enter');
-
-
-function agregarTarea(tarea){
-  const elemento = 
-  `
-  <li id="elemento">
-  <i class="far ${REALIZADO}" data="realizado" id="${id}"></i>
-  <p class="text ${LINE}">${tarea}</p>
-  <i class="fas fa-trash de" data="eliminado" id="${id}"></i> 
-  </li>
-  `
-  lista.insertAdjacentHTML("beforeend",elemento)
+function agregarTarea(nombre, id, realizado, eliminado) {
+  const REALIZADO = realizado ? 'fa-check-circle' : 'fa-circle-thin';
+  const LINE = realizado ? 'lineThrough' : '';
+  const elemento = `
+    <li id="${id}">
+      <i class="far ${REALIZADO}" data="realizado" id="${id}"></i>
+      <p class="text ${LINE}">${nombre}</p>
+      <i class="fas fa-trash de" data="eliminado" id="${id}"></i>
+    </li>
+  `;
+  lista.insertAdjacentHTML('beforeend', elemento);
 }
 
-botonEnter.addEventListener('click', ()=> {
-  const tarea = input.value
-  if(tarea){
-    
-    /*definir tareas*/
-
-      agregarTarea(tarea,id,false,false)
-      LIST.push({
-          nombre : tarea,
-          id : id,
-          realizado : false,
-          eliminado : false
-      })
-      localStorage.setItem('TODO',JSON.stringify(LIST))
-      id++
-      input.value = ''
+botonEnter.addEventListener('click', () => {
+  const tarea = input.value;
+  if (tarea) {
+    agregarTarea(tarea, id, false, false);
+    LIST.push({
+      nombre: tarea,
+      id: id,
+      realizado: false,
+      eliminado: false,
+    });
+    localStorage.setItem('TODO', JSON.stringify(LIST));
+    id++;
+    input.value = '';
   }
-
-})
+});
