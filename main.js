5388c2b99a3bfbce18531d9cfc15c09430f1d306
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-05-02 14:13:28 -0300

    //2/5/2023-14:13

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 const LIST = [];
 let id = 0;
 
@@ -5,31 +6,25 @@ const lista = document.querySelector('#lista');
 const input = document.querySelector('#input');
 const botonEnter = document.querySelector('#boton-enter');
 
-function agregarTarea(nombre, id, realizado, eliminado) {
-  const REALIZADO = realizado ? 'fa-check-circle' : 'fa-circle-thin';
-  const LINE = realizado ? 'lineThrough' : '';
-  const elemento = `
-    <li id="${id}">
-      <i class="far ${REALIZADO}" data="realizado" id="${id}"></i>
-      <p class="text ${LINE}">${nombre}</p>
-      <i class="fas fa-trash de" data="eliminado" id="${id}"></i>
+function agregarTarea(tarea){
+  const elemento =
+  `
+    <li id="elemento">
+      <i class="far fa-circle co" data="realizado" id="0"></i>
+      <p class="text ">${tarea}</p>
+      <i class="fas fa-trash de" data="eliminado" id="0"></i>
     </li>
-  `;
-  lista.insertAdjacentHTML('beforeend', elemento);
+  `
+  lista.insertAdjacentHTML("afterend",elemento)
 }
+
+
 
 botonEnter.addEventListener('click', () => {
   const tarea = input.value;
   if (tarea) {
-    agregarTarea(tarea, id, false, false);
-    LIST.push({
-      nombre: tarea,
-      id: id,
-      realizado: false,
-      eliminado: false,
-    });
-    localStorage.setItem('TODO', JSON.stringify(LIST));
-    id++;
+    agregarTarea(tarea)
     input.value = '';
   }
 });
+ 
