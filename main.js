4bb2fe1e298fbe602b6128c9946bbacbe20e3449
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-05-03 08:37:57 -0300

    //3/05/2023-8.36

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
-const LIST = [];
+const LIST = [];// constante LIST  (array vacío)
+const lista = document.querySelector('#lista');// busca el id lista y lo mete en la variable
+const input = document.querySelector('#input');// busca el id input y lo mete en la variable
+const botonEnter = document.querySelector('#boton-enter');// busca el id boton-enter y lo mete en la variable
+const check = 'fa-check-circle'
 let id = 0;
 
-const lista = document.querySelector('#lista');
-const input = document.querySelector('#input');
-const botonEnter = document.querySelector('#boton-enter');
-const check='fa-check-circle'
-
-function agregarTarea(tarea){
+function agregarTarea(tarea) {
   const elemento =
-  `
+    `
     <li id="elemento">
       <i class="far fa-circle co" data="realizado" id="0"></i>
       <p class="text ">${tarea}</p>
       <i class="fas fa-trash de" data="eliminado" id="0"></i>
     </li>
   `
-  lista.insertAdjacentHTML("afterend",elemento)
-  LIST.push(tarea); // Agrega la tarea al array LIST
-  console.log(LIST); // Muestra el array LIST en la consola
+  lista.insertAdjacentHTML("afterend", elemento)
+  LIST.push(tarea); // Agrega  tarea nueva al array 
+  console.log(LIST); // array en la consola
 }
 
 botonEnter.addEventListener('click', () => {
@@ -28,9 +27,9 @@ botonEnter.addEventListener('click', () => {
   }
 });
 
-document.addEventListener('keyup', function(event){
-  if(event.key=='Enter'){
-    const tarea= input.value
+document.addEventListener('keyup', function (event) {
+  if (event.key == 'Enter') {
+    const tarea = input.value
     if (tarea) {
       agregarTarea(tarea);
       input.value = '';
