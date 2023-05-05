81fe91093fcbcadff17a9b50a1cd086d3b808bcd
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-05-05 07:29:29 -0300

    //5/05/2023-07.29

--- calculadora.js
+++ calculadora.js
@@ -2,7 +2,6 @@ const LIST = []; // constante LIST (array vacío)
 const lista = document.querySelector('#lista'); // busca el id lista y lo mete en la variable
 const input = document.querySelector('#input'); // busca el id input y lo mete en la variable
 const botonEnter = document.querySelector('#boton-enter'); // busca el id boton-enter y lo mete en la variable
-
 let id = 0;
 
 function agregarTarea(tarea) {
@@ -10,20 +9,18 @@ function agregarTarea(tarea) {
     `<div class="flex items-center" style="; padding-right: 40px; flex: 1; margin-right: 16px;padding-right: 8px; font-size: 16px;border: none; ;">
       <li class="tarea flex justify-between items-center" style="list-style: none; display: flex; justify-content: flex-end;   justify-content: space-between; margin: 5%;">
         <p class="text">${tarea}</p>
-        <div style="display: flex;margin-top: 20px; " >
-          <i class="far fa-circle co" data-realizado="false";style="margin-right:10; "></i>
+        <div style="display: flex;margin-top: 20px; " ><button class="comfirmacion co" data-realizado="false" style="margin-right:10;">Confirmar</button>
           <i class="fas fa-trash de" data-eliminado="false";style="margin-right:10;" ></i>
+          
         </div>
       </li>
-    </div>`;
-  
-  lista.insertAdjacentHTML("afterend", elemento);
+    </div>
+    `;
+lista.insertAdjacentHTML("afterend", elemento);
   LIST.push({ tarea: tarea, realizado: false, id: id }); // agrega tarea nueva al array con su id y estado realizado en false
-  console.log(LIST); // se ve el array en la consola
-  
+  console.log(LIST); // se ve el array en la consola  
   id++;
 }
-
 botonEnter.addEventListener('click', () => {
   const tarea = input.value;
   if (tarea) {
@@ -31,7 +28,6 @@ botonEnter.addEventListener('click', () => {
     input.value = '';
   }
 });
-
 document.addEventListener('keyup', function (event) {
   if (event.key == 'Enter') {
     const tarea = input.value;
@@ -41,18 +37,12 @@ document.addEventListener('keyup', function (event) {
     }
   }
 });
-
-// escucha el boton comfirmacion y cambia su booleano
-document.addEventListener('click', function (event) {
-    if (event.target && event.target.classList.contains('co')) {
-      const index = LIST.findIndex(item => item.id === parseInt(event.target.parentElement.parentElement.getAttribute('data-id')));
-      LIST[index].realizado = true; // cambia el valor booleano de realizado a true
-      event.target.classList.add('fa-check-circle');
-      event.target.classList.remove('fa-circle');
-      event.target.setAttribute('data-realizado', 'true');
-      console.log(LIST[index].realizado); // el  boleano actualizado en consola
-    }
-  });
+if (event.target && event.target.classList.contains('co')) {
+  const index = LIST.findIndex(item => item.id === parseInt(event.target.parentElement.parentElement.getAttribute('id')));
+  LIST[index].realizado = true; // cambi BOOLEANO A  TRUE
+  event.target.setAttribute('data-realizado', 'true'); // Se actualiza "data-realizado"
+  console.log(LIST);
+}
 
 // escucha el boton de eliminación y elimina el array y el elemento html
 document.addEventListener('click', function (event) {
--- main.js
+++ main.js
@@ -1,4 +1,63 @@
-const LIST = [];// constante LIST  (array vacío)
+const LIST = []; // constante LIST (array vacío)
+const lista = document.querySelector('#lista'); // busca el id lista y lo mete en la variable
+const input = document.querySelector('#input'); // busca el id input y lo mete en la variable
+const botonEnter = document.querySelector('#boton-enter'); // busca el id boton-enter y lo mete en la variable
+let id = 0;
+
+function agregarTarea(tarea) {
+  const elemento =
+    `<div class="flex items-center" style="; padding-right: 40px; flex: 1; margin-right: 16px;padding-right: 8px; font-size: 16px;border: none; ;">
+      <li class="tarea flex justify-between items-center" id="${id}" style="list-style: none; display: flex; justify-content: flex-end;   justify-content: space-between; margin: 5%;">
+        <p class="text">${tarea}</p>
+        <div style="display: flex;margin-top: 20px; " >
+          <button class="confirmacion" style="margin-right:10;">Confirmar</button>
+          <i class="fas fa-trash de" data-eliminado="false" style="margin-right:10;"></i>
+        </div>
+      </li>
+    </div>
+    `;
+  lista.insertAdjacentHTML("afterend", elemento);
+  LIST.push({ tarea: tarea, realizado: false, id: id }); // agrega tarea nueva al array con su id y estado realizado en false
+  console.log(LIST); // se ve el array en la consola  
+  id++;
+}
+
+botonEnter.addEventListener('click', () => {
+  const tarea = input.value;
+  if (tarea) {
+    agregarTarea(tarea);
+    input.value = '';
+  }
+});
+
+document.addEventListener('keyup', function (event) {
+  if (event.key == 'Enter') {
+    const tarea = input.value;
+    if (tarea) {
+      agregarTarea(tarea);
+      input.value = '';
+    }
+  }
+});
+
+// escucha el botón de confirmación y cambia el valor booleano en el array
+document.addEventListener('click', function (event) {
+  if (event.target && event.target.classList.contains('confirmacion')) {
+    const index = LIST.findIndex(item => item.id === parseInt(event.target.parentElement.parentElement.getAttribute('id')));
+    LIST[index].realizado = true; // cambia el valor booleano en el array a true
+    console.log(LIST);
+  }
+  
+  // escucha el botón de eliminación y elimina el array y el elemento html
+  if (event.target && event.target.classList.contains('de')) {
+    const index = LIST.findIndex(item => item.id === parseInt(event.target.parentElement.parentElement.getAttribute('id')));
+    LIST.splice(index, 1); // elimina la tarea del array
+    event.target.parentElement.parentElement.remove(); // elimina el elemento HTML correspondiente
+    console.log(LIST);
+  }
+})
+
+/* const LIST = [];// constante LIST  (array vacío)
 const lista = document.querySelector('#lista');// busca el id lista y lo mete en la variable
 const input = document.querySelector('#input');// busca el id input y lo mete en la variable
 const botonEnter = document.querySelector('#boton-enter');// busca el id boton-enter y lo mete en la variable
@@ -24,7 +83,7 @@ function agregarTarea(tarea) {//funcion para crear el html necesario para ver la
   console.log(LIST); // se ve el array en la consola
   /*agregar el boolean del circulo de confirmacion en false y que cambie a true cuando se oprime el boton
   y que sea visible desde la consola*/
-}
+/* 
 
 botonEnter.addEventListener('click', () => {//escucha el clicl del mouse y envia la nueva tarea a la funcion
   const tarea = input.value;
@@ -42,7 +101,7 @@ document.addEventListener('keyup', function (event) {//escucha cuando se oprime 
       input.value = '';
     }
   }
-})
+}) */ 
 
 /* //agregar funcion a boton eliminar y a boton confirmar
 const botonesEliminar = document.querySelectorAll('.de');
